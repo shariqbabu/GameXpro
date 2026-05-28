56f08b90a7cb8cd30c964348300cc45e906ad036
shrqbabu <techloot1100@gmail.com>
2026-05-28 07:44:54 +0530

    Delete src directory

--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { StatCard } from './StatCard';
-export { GlowButton } from './GlowButton';
-export { LiveActivityWidget } from './LiveActivityWidget';
-export { SkeletonCard, SkeletonTable, SkeletonText } from './SkeletonLoader';
